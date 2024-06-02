f2074b369ffa568c9f3e1406f0abcf6093ab2f5a
martincaisley <martincaisley@hotmail.co.uk>
2024-06-02 19:38:09 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define SIZE 3
+
+// Prototypes
+bool checkForWin(char board[SIZE][SIZE], char player);
+bool checkForDraw(char board[SIZE][SIZE]);
+void printGrid(char board[SIZE][SIZE]);
+void makeMove(int move, char player, char board[SIZE][SIZE]);
+
+void printGrid(char board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < SIZE - 1) printf("|");
+        }
+        printf("\n");
+        if (i < SIZE - 1) printf("---+---+---\n");
+    }
+}
+
+void makeMove(int move, char player, char board[SIZE][SIZE]) {
+    int row = (move - 1) / SIZE;
+    int col = (move - 1) % SIZE;
+    board[row][col] = player;
+}
+
+bool checkForDraw(char board[SIZE][SIZE]) {
+    for (int i = 0 ; i < SIZE ; i++) {
+        for (int j = 0 ; j < SIZE ; j++) {
+            if(board[i][j] != 'x' && board[i][j] != 'o' ) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool checkForWin(char board[SIZE][SIZE], char player) {
+    // Orthogonal.
+    for (int i = 0; i < SIZE; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
+    // Diagonal
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+    return false;
+}
+
+int main(void) {
+    bool gameWon = false;
+    bool gameDrawn = false;
+    char currentPlayer = 'x';
+    char move;
+
+    char board[SIZE][SIZE] = {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+    };
+
+    while(!gameWon && !gameDrawn) {
+        printf("Player %c, enter your move using the numbers on the grid below:\n\n", currentPlayer);
+
+        printGrid(board);
+        scanf(" %c", &move); // The space before %c to consume any leftover whitespace.
+        int moveNumber = move - '0';
+
+        if (moveNumber < 1 || moveNumber > 9 ) {
+            printf("Invalid Move Number: %c, Please try again\n", move);
+            continue; // Skip to the next iteration of the loop to get a valid move
+        }
+
+        makeMove(moveNumber, currentPlayer, board);
+        printGrid(board);
+        gameWon = checkForWin(board, currentPlayer);
+        gameDrawn = checkForDraw(board);
+
+        if(gameWon) {
+            printf("Player %c has won!\n", currentPlayer);
+        } else if(gameDrawn) {
+            printf("Game ended in a draw\n");
+        } else {
+            currentPlayer = (currentPlayer == 'x') ? 'o' : 'x';
+        }
+    }
+    return 0;
+}
